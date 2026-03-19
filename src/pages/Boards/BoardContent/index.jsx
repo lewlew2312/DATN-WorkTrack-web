3c6b3114ae75587500f10714297d388347246e14
unknown <phamgiatien.nd2002@gmail.com>
2026-03-19 21:15:33 +0700

    Refactor Project Structure

---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box'
+
+function BoardContent() {
+  return (
+    <Box sx={{ backgroundColor: 'primary.main', width: '100%', height: (theme) => `calc(100vh - ${theme.worktrackCustom.appBarHeight} - ${theme.worktrackCustom.boardBarHeight})`, display: 'flex', alignItems: 'center' }}>
+        Board Content
+    </Box>
+  )
+}
+
+export default BoardContent
